00099df299c25ff19e60ceefbcccf8df38adb33d
Matt Whiteley <thatmattwhiteley@gmail.com>
2023-03-23 16:31:34 +0000

    Staging models, cleaning and ready to join

--- synth_dbt/models/staging/stg_seed__pagesviews.sql
+++ synth_dbt/models/staging/stg_seed__pagesviews.sql
@@ -1,12 +1,17 @@
-with pages_views as (
+WITH pages_views as (
     SELECT * from {{ref ('pages_views')}}
+),
+
+rename_decodeGUID AS (
+    SELECT
+        pv.USER_ID as USER_ID_GUID,
+        --Convert GUIDs to the shorter Base64 format, to match UserIDs
+        to_base64(SAFE_CAST(REPLACE(USER_ID,"-","") AS BYTES FORMAT 'HEX')) as USER_ID_BASE64,
+        -- Rename timestamp for clarity on timezone, separate date for day-only calcs
+        pv.RECEIVED_AT as RECEIVED_AT_UTC,
+        SAFE_CAST(RECEIVED_AT AS DATE) as RECEIVED_AT_DATE_UTC,
+        pv.NAME as PAGE_LOCATION
+    FROM pages_views pv
 )
 
--- rename to UTC
--- format USER IDs
-
-SELECT
-    pv.USER_ID,
-    pv.RECEIVED_AT,
-    pv.NAME
-FROM pages_views pv
+SELECT * FROM rename_decodeGUID
--- synth_dbt/models/staging/stg_seed__users.sql
+++ synth_dbt/models/staging/stg_seed__users.sql
@@ -1,13 +1,15 @@
 with users as (
     SELECT * from {{ref ('users')}}
-)
+),
 
--- CReated AT datestamps
--- rename UTC
-
-SELECT
-    u.USER_ID,
-    u.CREATED_AT,
+rename_convertDates as (SELECT
+    u.USER_ID as USER_ID_BASE64,
+    --Convert string timestamp to DATETIME and DATE formats
+    PARSE_DATETIME('%FT%R:%E*SZ',u.CREATED_AT) as CREATED_AT_UTC,
+    SAFE_CAST(PARSE_DATETIME('%FT%R:%E*SZ',u.CREATED_AT) as DATE) as CREATED_AT_DATE_UTC,
     u.SEGMENT,
     u.IS_INTERNAL
 FROM users u
+)
+
+select * from rename_convertDates
